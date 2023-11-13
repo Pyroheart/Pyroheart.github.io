709a8e1a96a727cda4a364198eb11daab8505155
Samuel A <sam.demon@live.fr>
2023-11-13 14:18:32 +0100

    Delete src directory

--- src/data/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = {
-    profile: "/selectProfile",
-    browse: "/browse",
-}
-
-
-export default routes;
--- src/services/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const networkService = axios.create({
-    baseURL:"https://api.themoviedb.org/3"
-});
-
-export const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
-
-export default networkService;
